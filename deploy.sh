aec3218c2305638c5a29d9edb9aecfb9b2fbc3fe
srun-soknoeurn <soknoeurn.srun168@gmail.com>
2023-11-20 01:01:14 +0700

    change way to deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:srun-soknoeurn/srun-soknoeurn.github.io.git main
+# git push -f git@github.com:srun-soknoeurn/srun-soknoeurn.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:srun-soknoeurn/techbodia-assignment.git main:gh-pages
+git push -f git@github.com:srun-soknoeurn/techbodia-assignment.git main:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 // vue.config.js file to be placed in the root of your repository
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/techbodia-assignment/'
-      : '/'
-  }
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/techbodia-assignment/" : "/",
+};

020fdb564efae4d69292f7591ac3121b8ff6f213
srun-soknoeurn <soknoeurn.srun168@gmail.com>
2023-11-19 10:27:01 +0700

    add search and sortby function

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,121 @@
-<template >
+<template>
   <div>
-    <CatalogItems/>
+    <header class="position-relative p-1 mb-4 position-sticky z-1 top-0">
+      <img src="./assets/download.jpeg" alt="" width="70" />
+      <h3
+        class="position-absolute top-50 start-50 translate-middle text-center"
+      >
+        TECHBODIA Web Developer Coding Assignment
+      </h3>
+    </header>
+    <main class="container">
+      <SearchFlagItems />
+
+      <div class="mb-2">
+        <div class="col-4">
+          <input
+            class="form-control"
+            type="search"
+            v-model="search"
+            @keyup="SearchByCountryName(search)"
+            placeholder="Search For Country Name..."
+            aria-label="Recipient's username"
+            aria-describedby="basic-addon2"
+          />
+        </div>
+        <div class="sort">
+          Sorting by:
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="flexRadioDefault"
+              id="az"
+              v-model="checked"
+              @click="sortData(checked)"
+              :value="checked"
+            />
+            <label class="form-check-label" for="az"> A-Z </label>
+          </div>
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="radio"
+              name="flexRadioDefault"
+              id="za"
+              :value="false"
+              @click="sortData(false)"
+            />
+            <label class="form-check-label" for="za"> Z-A </label>
+          </div>
+        </div>
+      </div>
+      <CatalogItems :data="data" />
+    </main>
   </div>
 </template>
 <script>
-import CatalogItems from './components/CatalogItems.vue';
+import SearchFlagItems from "./components/SearchFlagItems.vue";
+import CatalogItems from "./components/CatalogItems.vue";
 export default {
   components: {
-    CatalogItems
+    CatalogItems,
+    SearchFlagItems,
   },
-}
+  data() {
+    return {
+      API: "https://restcountries.com/v3.1/all",
+      data: [],
+      checked: true,
+      search: "",
+    };
+  },
+  methods: {
+    sortData(checked) {
+      if (checked) {
+        // Sort in ascending order
+        this.data = this.data.sort((a, b) =>
+          a.name.official.localeCompare(b.name.official)
+        );
+      } else {
+        this.data = this.data.sort((a, b) =>
+          b.name.official.localeCompare(a.name.official)
+        );
+      }
+    },
+    SearchByCountryName(query) {
+      if (query) {
+        this.data  = this.data.filter((item) =>
+          item.name.official.toLowerCase().includes(query.toLowerCase())
+        );
+      } else {
+        this.listFlagsHandler();
+
+      }
+    },
+    listFlagsHandler() {
+      fetch(this.API)
+        .then((res) => res.json())
+        .then((data) => {
+          this.data = data;
+          this.sortData(this.checked);
+        });
+    },
+  },
+  mounted() {
+    this.listFlagsHandler();
+  },
+};
 </script>
 <style scoped>
-  
+header {
+  background: #282828;
+}
+h3 {
+  color: #ff007a;
+}
+.vertical-line {
+  border-left: 2px solid black;
+  height: 100px; /* Adjust the height as needed */
+}
 </style>
--- src/components/CatalogItems.vue
+++ src/components/CatalogItems.vue
@@ -1,85 +1,55 @@
 <template>
-  <div class="row row-cols-4 row-cols-md-3 g-4">
-    <div class="col" v-for="flag in data" :key="flag">
-      <div class="card h-100">
-        <img :src="flag.flags.png" class="card-img-top h-75" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">{{ flag.name.official }}</h5>
-          <ul>
-            <li>{{ flag.cca2 }}</li>
-            <li>{{ flag.cca3 }}</li>
-            <li>{{ flag.name.nativeName }}</li>
-            <li>{{ flag.altSpellings }}</li>
-            <li class="p-0 m-0">
-              <span>{{ flag.idd.root }}</span> 
-              <span v-for="item in flag.idd.suffixes" :key="item">
-                {{ item }}</span
-              >
-            </li>
-          </ul>
+  <div>
+  <div v-if="data.length > 0">
+    <div class="card mb-3 bg-card p-1" v-for="flag in data" :key="flag">
+      <div class="row">
+        <div class="col-md-3">
+          <img
+            :src="flag.flags.png"
+            class="img-fluid rounded-start w-100 h-100"
+            alt="..."
+          />
+        </div>
+        <div class="col-md-9">
+          <div class="card-body">
+            <h5 class="card-title">{{ flag.name.official }}</h5>
+            <ul>
+              <li>{{ flag.cca2 }}</li>
+              <li>{{ flag.cca3 }}</li>
+              <li>{{ flag.name.nativeName }}</li>
+              <li>{{ flag.altSpellings }}</li>
+              <li class="p-0 m-0">
+                <span>{{ flag.idd.root }}</span>
+                <span v-for="item in flag.idd.suffixes" :key="item">
+                  {{ item }}</span
+                >
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-    <!-- <div class="col">
-      <div class="card">
-        <img src="..." class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card">
-        <img src="..." class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content.
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card">
-        <img src="..." class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
-          </p>
-        </div>
-      </div>
-    </div> -->
+  </div>
+  <div v-else>
+    <h1>Not found.</h1>
+  </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      API: "https://restcountries.com/v3.1/all",
-      data: [],
-    };
-  },
-  methods: {
-    listFlagsHandler() {
-      fetch(this.API)
-        .then((res) => res.json())
-        .then((data) => {
-          this.data = data;
-        });
-    },
-  },
-  mounted() {
-    this.listFlagsHandler();
-  },
+  props: ["data"],
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.bg-card {
+  background: rgba(0, 0, 0, 0.1);
+  transition: box-shadow 0.1s ease, transform 0.3s ease-in-out;
+}
+.bg-card:hover {
+  /* background: rgb(254, 54, 54); */
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+  transform: scale(1.01);
+}
+</style>
